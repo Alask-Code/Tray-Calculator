040c33b2f69c0d666a87b290fd3b3801b22a278c
alask-code <anonimoplayer736@gmail.com>
2023-09-09 06:34:46 -0300

    hotfix

--- electron/Tray.js
+++ electron/Tray.js
@@ -1,6 +1,6 @@
 const { Tray } = require('electron')
 const { resolve } = require('path')
-const icon = resolve(__dirname, '../', 'build', 'icon.png')
+const icon = resolve(__dirname, '../', 'assets', 'icon.png')
 function createTray() {
     const tray = new Tray(icon)
     tray.setToolTip('Click to open calculator, Right-click to close.')
